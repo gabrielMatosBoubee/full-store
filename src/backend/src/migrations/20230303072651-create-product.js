5691893dbd50ebe4bd7324c06783885e8730257a
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-03-03 04:54:33 -0300

    add product seed

--- src/backend/src/migrations/20230303072651-create-product.js
+++ src/backend/src/migrations/20230303072651-create-product.js
@@ -18,7 +18,8 @@ module.exports = {
         type: Sequelize.STRING
       },
       discount_percent: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        defaultValue: 0
       },
       createdAt: {
         allowNull: false,
