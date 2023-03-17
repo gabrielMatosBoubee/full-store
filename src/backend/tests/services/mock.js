d4fbe3270ef9937420a4909f5ff355d1ce0956f4
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-03-17 02:31:09 -0300

    test: services user tests

---
+++ src/backend/tests/services/mock.js
@@ -0,0 +1,10 @@
+const createUser = {
+    dataValues: {
+        "email": "test@gmail.com",
+        "username": "John Doe",
+        "password": "Te12345678!",
+        "id": 1
+    }
+}
+
+module.exports = { createUser }
---
+++ src/backend/tests/services/user.test.js
@@ -0,0 +1,102 @@
+const sinon = require('sinon');
+const { user } = require('../../src/services')
+const { User } = require('../../src/models');
+const auth = require('../../src/auth/auth')
+const { expect } = require('chai');
+const mock = require('./mock')
+
+
+describe('Tests user crud', function () {
+    it('Test user create generate a token right', async function () {
+        sinon.stub(User, 'create').resolves(mock.createUser)
+
+        const { type, message } = await user.insertUser({
+            email: "test@gmail.com",
+            username: "John Doe",
+            password: "Te12345678!",
+        });
+        const result = auth.verifyToken(message);
+        expect(type).to.be.equal(201);
+        expect(result).to.be.deep.equal(mock.createUser.dataValues);
+        expect(typeof message).to.be.equal('string');
+    });
+    it('Test user login', async function () {
+        sinon.stub(User, 'findOne').resolves(mock.createUser)
+
+        const { type, message } = await user.login({
+            email: "test@gmail.com",
+            password: "Te12345678!",
+        })
+
+        expect(type).to.be.equal(200);
+        expect(typeof message).to.be.equal('string');
+    })
+
+    it("Test user login send error message if does'nt exist the user", async function () {
+        sinon.stub(User, 'findOne').resolves(null)
+
+        const { type, message } = await user.login({
+            email: "testError@gmail.com",
+            password: "Te12345678!",
+        })
+
+        expect(type).to.be.equal(404);
+        expect(message).to.be.deep.equal({ message: 'User not found or not exist' });
+    })
+
+    it("Test user login send error message if receive wrong password", async function () {
+        sinon.stub(User, 'findOne').resolves(mock.createUser);
+
+        const { type, message } = await user.login({
+            email: "test@gmail.com",
+            password: "Te11111111111!",
+        });
+
+        expect(type).to.be.equal(400);
+        expect(message).to.be.deep.equal({ message: 'Wrong password' });
+    })
+    it("Test user update", async function () {
+        sinon.stub(User, 'update').resolves(mock.createUser);
+
+        const { type, message } = await user.update({
+            email: "test@gmail.com",
+            password: "Teste11111111111!"
+        });
+
+        expect(type).to.be.equal(200);
+        expect(message).to.be.deep.equal(mock.createUser)
+    })
+    it("Test user update send error if the email doen't", async function () {
+        sinon.stub(User, 'findOne').resolves(null);
+
+        const { type, message } = await user.update({
+            email: "testError@gmail.com",
+            password: "Te12345678!",
+        });
+
+        expect(type).to.be.equal(404)
+        expect(message).to.be.deep.equal({ message: 'User not found or not exist' })
+    })
+    it("Test user delete", async function () {
+        sinon.stub(User, 'delete').resolves(mock.createUser);
+
+        const { type, message } = await user.delete({
+            email: "test@gmail.com",
+            password: "Teste11111111111!"
+        });
+
+        expect(type).to.be.equal(204);
+        expect(message).to.be.deep.equal([])
+    })
+    it("Test user delete send error if the email doen't", async function () {
+        sinon.stub(User, 'findOne').resolves(null);
+
+        const { type, message } = await user.delete({
+            email: "testError@gmail.com",
+            password: "Te12345678!",
+        });
+
+        expect(type).to.be.equal(404)
+        expect(message).to.be.deep.equal({ message: 'User not found or not exist' })
+    })
+});
